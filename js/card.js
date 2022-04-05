da22aeeaf933c3491852bd45a0aa0313f8bac01f
Вадим Молчанов <vadim.molchanov@gmail.com>
2022-04-05 22:35:56 +0300

    fix lint

--- js/card.js
+++ js/card.js
@@ -4,7 +4,7 @@ const AD_AMOUNT = 10;
 
 const tenFakeAds = Array.from({length:AD_AMOUNT}, createAd);
 
-// tenFakeAds();
+tenFakeAds();
 
 const cardTemplate = document.querySelector('#card').querySelector('.popup');
 
